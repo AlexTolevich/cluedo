078e5e3032202940388ce282c0e4385c0df279ac
Alexander Kuznetsov <alexanderanatolevic@yandex.ru>
2023-04-26 23:32:24 +0500

    configure webpack to work with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 const title = 'CLUEDO';
 
 const root = createRoot(document.getElementById('app'));
 
-root.render(<h1>{title}</h1>)
+root.render(<h1>{title}</h1>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,56 @@
 const path = require('path');
-const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
+  plugins: [new ESLintPlugin()],
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            {
-                test: /\.css$/i,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader'
-                    },
-                ],
-            },
-            {
-                test: /\.(woff|woff2)$/,
-                type: 'asset/resource',
-            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        static: path.join(__dirname, './dist'),
-        compress: true,
-        port: 3000,
-        hot: true
-    },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, './dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
 };
